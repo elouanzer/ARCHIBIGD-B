b3f53d745acb49fb882224c821164b05aeae7af9
elouanzer <elouan.marsot83@gmail.com>
2025-02-21 17:13:22 +0100

    consumer

--- consumer/app/consumer.py
+++ consumer/app/consumer.py
@@ -5,59 +5,49 @@ from kafka import KafkaAdminClient, KafkaConsumer
 
 def consumer_from_kafka(topic,stations):
 
-    stations_bikes={}
-    stations_date={}
+    parkings={}
+    parking_name={}
 
-    stations={}
-    
+    consumer = KafkaConsumer("parking", bootstrap_servers='localhost:9092',auto_offset_reset="earliest", enable_auto_commit=True, auto_commit_interval_ms=1000)
+    consumer.subscribe(["parking"])
 
-        
-    consumer = KafkaConsumer("velib-stations", bootstrap_servers='localhost:9092',auto_offset_reset="earliest", enable_auto_commit=True, auto_commit_interval_ms=1000)
-    consumer.subscribe(["velib-stations"])
-
-    city_stations={}
-    # if consumer.empt
     for message in consumer:
-        station = json.loads(message.value.decode())
+        parking = json.loads(message.value.decode())
         #print(station)
         #print("da",datetime.utcfromtimestamp(station["last_update"]/1000).strftime('%Y-%m-%d %H:%M:%S'))
-        station_number = station["number"]
-        station_last_update = station["last_update"]
+        parking_id = parking['grp_identifiant']
+        parking_name = parking["grp_nom"]
+        availability = parking["disponibilite"]
 
-        #if station_number ==30:
-        #    print(station)
-        name = station["name"]
-        available_bike_stands = station["available_bike_stands"]
-            
-        if name not in city_stations:
-            city_stations[name] = available_bike_stands
+        if parking_name not in parkings:
+            parkings[parking_name] = availability
             #print("st",station_number, city_stations[station_number])
 
-        count_diff = available_bike_stands - city_stations[name]
+        count_diff = availability - parkings[parking_name]
         if count_diff != 0:
             #print(available_bike_stands,city_stations[station_number])
-            city_stations[name] = available_bike_stands
-            print("{}{} {} ({})".format(
-                "+" if count_diff > 0 else "",
-                count_diff, station["address"], name
-            ))
+            parkings[parking_name] = availability
+            if count_diff > 0:
+                print('+', count_diff, parking["grp_nom"])
+            else :
+                print(count_diff, parking["grp_nom"])
         #else:
             #print("0",station["address"])
-    
+
     consumer.close()
 
 
 
 def main():
     print("consumer : ")
-    stations = {}
-    topic = 'velib-stations'
+    parkings = {}
+    topic = 'parking'
     try:
         admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
     except Exception:
         pass
 
-    consumer_from_kafka(topic,stations)
+    consumer_from_kafka(topic,parkings)
 
 if __name__ == "__main__":
     main()
