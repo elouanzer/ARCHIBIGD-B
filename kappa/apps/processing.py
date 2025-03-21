fd08446f371e87986f23b08c5ce8e16522708f9e
elouanzer <elouan.marsot83@gmail.com>
2025-03-21 15:17:39 +0100

    akjedb

--- kappa/apps/processing.py
+++ kappa/apps/processing.py
@@ -1,22 +1,25 @@
-"""
-Demo Spark Structured Streaming + Apache Kafka + Cassandra
-"""
-
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField,LongType,IntegerType,FloatType,StringType
 from pyspark.sql.functions import split,sum,from_json,col
-
-
 import sys
 
 def main():
 
-    capteurSchema = StructType([
-                StructField("date",StringType(),False),
-                StructField("numero",IntegerType(),False),
-                StructField("capteur",StringType(),False),
-                StructField("valeur",FloatType(),False)
-            ])
+    message_schema = StructType([
+        StructField("grp_identifiant", StringType(), False),
+        StructField("grp_nom", StringType(), False),
+        StructField("grp_statut", IntegerType(), False),
+        StructField("grp_disponible", IntegerType(), False),
+        StructField("grp_exploitation", IntegerType(), False),
+        StructField("grp_complet", IntegerType(), False),
+        StructField("grp_horodatage", StringType(), False),
+        StructField("idobj", StringType(), False),
+        StructField("location", StructType([
+            StructField("lon", DoubleType(), False),
+            StructField("lat", DoubleType(), False)
+        ]), False),
+        StructField("disponibilite", IntegerType(), False)
+    ])
 
     spark = SparkSession.builder \
         .appName("Spark-Kafka-Cassandra") \
@@ -36,8 +39,22 @@ def main():
         .option("delimeter",",") \
         .option("startingOffsets", "latest") \
         .load()
+    '''
+    df1 = df \
+        .selectExpr("CAST(value AS STRING) as json") \
+        .select(from_json(col("json"), message_schema).alias("data")) \
+        .select("data.*")
+    '''
+    df1 = df.select(
+        col("timestamp"),
+        from_json(col("value").cast("string"), message_schema).alias("data")
+    ).select("timestamp", "data.*")
 
-    df1 = df.selectExpr("CAST(value AS STRING)").select(from_json(col("value"),capteurSchema).alias("data")).select("data.*")
+    df1.printSchema()
+
+    df1.filter(col("grp_statut") > 0)
+    df1 = df1.withColumn("dispo_pourcentage", (max(0,col("grp_disponible")) / col("grp_exploitation")) * 100)
+
     df1.printSchema()
 
     def writeToCassandra(writeDF, epochId):
